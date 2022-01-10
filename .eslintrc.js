0c50ca2d5fe497e53cf4f78055ac9a4ba8fda034
Mims H. Wright <mims@mimswright.com>
2022-01-10 23:30:51 +0100

    fixed issues with linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
     "eslint:recommended",
     "plugin:jsx-a11y/recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
     "plugin:prettier/recommended",
     "plugin:ramda/recommended",
   ],
